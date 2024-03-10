9326b435861a2d9f8b498173e4f373fa636a57b3
Gautam kumar Singh <146578992+Gautamkumarsinghh@users.noreply.github.com>
2024-03-11 01:47:16 +0530

    Create project1.java
    This is a simple code in find a even or odd number in java.

---
+++ project1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+import java.util.Scanner;
+
+public class conditional {
+    // conditional Statements: if,else, else if, switch, break
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if (x % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
