b3a9d66bd2d23838dd74260424d442e3601b1892
Gautam kumar Singh <146578992+Gautamkumarsinghh@users.noreply.github.com>
2024-03-11 00:53:07 +0530

    Create index.java
    Basic learn in java journey

---
+++ index.java
@@ -0,0 +1,43 @@
+
+class index {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+
+        {
+            System.out.println("*");
+            System.out.println("**");
+            System.out.println("***");
+            System.out.println("****");
+            System.out.println("*****");
+            System.out.println("******");
+            System.out.println("*******");
+            System.out.println("********");
+        }
+
+        {
+            // variable
+
+            int a = 4;
+            int b = 4;
+            int Sum = a + b;
+            int diff = a - b;
+            int multi = a * b;
+            int div = a / b;
+
+            System.out.println(Sum);
+            System.out.println(diff);
+            System.out.println(multi);
+            System.out.println(div);
+
+        }
+
+        // Take in input java
+
+    }
+
+}
