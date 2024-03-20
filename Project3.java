f148a2f3100a6c060de2bae7772569521210968a
Gautam kumar Singh <146578992+Gautamkumarsinghh@users.noreply.github.com>
2024-03-20 23:26:13 +0530

    Update Project3.java

--- Project3.java
+++ Project3.java
@@ -4,7 +4,7 @@ public class conditional {
     public static void main(String arge[]) {
         Scanner sc = new Scanner(System.in);
         int button = sc.nextInt();
-
+//if else- if use code
         if (button == 1) {
             System.out.println("Hello");
         } else if (button == 2) {
@@ -14,6 +14,23 @@ public class conditional {
         } else {
             System.out.println("Invalid Button");
         }
+
+  //Switch case statement use code
+
+   switch (button) {
+            case 1:
+                System.out.println("Hello");
+                break;
+            case 2:
+                System.out.println("Namaste");
+                break;
+            case 3:
+                System.out.println("Welcome");
+                break;
+
+            default:
+                System.out.println("Invalid Button");
+     
     }
 
 }
